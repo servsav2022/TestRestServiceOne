fe80aa9c250c3414d2f7eb8b78d6e9e7b6f83271
alexandr <svaluhinav@yandex.ru>
2023-05-09 20:55:45 +0500

    Лаб 15, выполненные задания по 1,2 файлу

--- src/main/java/com/example/testrestserviceone/TestRestServiceOneApplication.java
+++ src/main/java/com/example/testrestserviceone/TestRestServiceOneApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestRestServiceOneApplication {
-//12
+
     public static void main(String[] args) {
         SpringApplication.run(TestRestServiceOneApplication.class, args);
     }
